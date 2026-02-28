936989dadefcfd05d3da8fc8bf308974c933d658
Akshita Gupta <gungunakshita37gupta973@gmail.com>
2026-02-28 19:49:25 +0530

    adding appointment

--- frontend-doctor/src/pages/Appointment.jsx
+++ frontend-doctor/src/pages/Appointment.jsx
@@ -1,7 +1,37 @@
+import { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom"
+import { AppContext } from "../context/AppContext";
 
 const Appointment = () => {
+
+  const {docId} = useParams();
+  const {doctors} = useContext(AppContext);
+  const [docInfo, setDocInfo] = useState(null);
+
+  const fetchDocInfo = async() => {
+    const docInfo = doctors.find(doc => doc._id === docId);
+    setDocInfo(docInfo);
+   // console.log(docInfo)
+  }
+
+  useEffect(() => {
+    fetchDocInfo();
+  }),[doctors, docId];
+
   return (
-    <div>Appointment</div>
+    <div>
+        {/* doctor details */}
+        <div>
+          <div>
+            <img src={docInfo.image} alt={docInfo.name} />
+          </div>
+          {/* doc info -  name, deg, exp */}
+          <div>
+            <p>{docInfo.name}</p>
+          </div>
+
+        </div>
+    </div>
   )
 }
 
