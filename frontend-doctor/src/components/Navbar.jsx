cb4e60279ae9400ac7affa04bf7c28d884e8427b
Akshita Gupta <gungunakshita37gupta973@gmail.com>
2026-02-28 08:11:34 +0530

    added some color changes

--- frontend-doctor/src/components/Navbar.jsx
+++ frontend-doctor/src/components/Navbar.jsx
@@ -13,21 +13,21 @@ const Navbar = () => {
     <div className="flex items-center justify-between text-md py-4 mb-4 border-b border-gray-600  ">
        <Logo />
        <div>
-        <ul className="hidden md:flex items-start gap-5 font-medium">
+        <ul className="hidden md:flex items-start gap-5 font-semibold">
             <NavLink to='/' >
-                <li className="py-1 text-blue-900 ">HOME</li>
+                <li className="py-1 text-amber-800 ">HOME</li>
                 <hr  className="border-none outline-none h-0.5 bg-gray-600 w-3/5 m-auto hidden " />
             </NavLink>
             <NavLink to='/doctors'>
-                <li className="py-1 text-blue-900 ">DOCTORS</li>
+                <li className="py-1 text-amber-800 ">DOCTORS</li>
                 <hr  className="border-none outline-none h-0.5 bg-gray-600 w-3/5 m-auto hidden " />
             </NavLink>
             <NavLink to='/about' >
-                <li className="py-1 text-blue-900 ">ABOUT</li>
+                <li className="py-1 text-amber-800">ABOUT</li>
                 <hr  className="border-none outline-none h-0.5 bg-gray-600 w-3/5 m-auto hidden " />
             </NavLink>
             <NavLink to='/contact' >
-                <li className="py-1 text-blue-900 ">CONTACT</li>
+                <li className="py-1 text-amber-800">CONTACT</li>
                 <hr  className="border-none outline-none h-0.5 bg-gray-600 w-3/5 m-auto hidden " />
             </NavLink>
         </ul>
@@ -37,14 +37,14 @@ const Navbar = () => {
             {
                 token ? <div className="flex items-center justify-center gap-2 group relative">
                     <span className="material-symbols-outlined 
-                        text-amber-700 bg-clip-text text-5xl drop-shadow-md  cursor-pointer
+                        text-blue-700 bg-clip-text text-5xl drop-shadow-md  cursor-pointer
                         shadow-lg rounded transition-transform duration-300 hover:scale-110 hover:drop-shadow-xl" style={{fontSize:"36px"}}>
                         account_circle
                     </span>
                     <span className="material-symbols-outlined text-emerald-700 cursor-pointer" style={{fontSize:"32px"}}>arrow_drop_down</span>
 
                     <div className="absolute top-0 right-0 pt-12 text-base font-medium text-gray-700 z-20 hidden group-hover:block">
-                        <div className="min-w-48 bg-stone-200 rounded flex flex-col cursor-pointer gap-4 p-4">
+                        <div className="min-w-48 bg-stone-200 rounded flex flex-col font-semibold cursor-pointer gap-4 p-4">
                             <p onClick={() => navigate('/my-profile')} className="flex items-center justify-center gap-1 hover:text-blue-800"><span className="material-symbols-outlined text-amber-800">switch_account</span> My Profile</p>
                             <p onClick={() => navigate('/my-appointment')} className="flex items-center justify-center gap-1 hover:text-blue-800"><span className="material-symbols-outlined text-amber-800">calendar_add_on</span> Appointments</p>
                             <p onClick={() => setToken(false)} className="flex items-center justify-center gap-1 hover:text-red-600"><span className="material-symbols-outlined text-amber-800">logout</span> Logout</p>
