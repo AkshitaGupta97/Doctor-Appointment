3c66232e357eb0b0da53634a8297bf745616f192
Akshita Gupta <gungunakshita37gupta973@gmail.com>
2026-03-02 19:24:02 +0530

    adding login

--- frontend-doctor/src/pages/Login.jsx
+++ frontend-doctor/src/pages/Login.jsx
@@ -1,7 +1,21 @@
+import { useState } from "react"
 
 const Login = () => {
+  const [state, setState] = useState('Sign Up');
+  
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [name, setName] = useState('');
+
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+    
+  }
+
   return (
-    <div>Login</div>
+    <form>
+      
+    </form>
   )
 }
 
